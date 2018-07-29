9987908b8afd69aea59c63de453f09799db5698b
Sk Ahmad <ahmad.sk98@gmail.com>
2018-07-30 01:14:21 +0530

    change database name,password table structure etc.

--- database/user_login/checkconn.php
+++ database/user_login/checkconn.php
@@ -2,8 +2,9 @@
 	$dbname = "contacts";
 	$usrtable = "users";
 	$contable = "persons";
-	
-	$conn = new mysqli("localhost","root","moon2light",$dbname);
+	//person table entity type [ name varchar(33), phone varchar(10), email varahr(32) ]
+	// user table entity type [ username varchar(32), authkey varchar(128), email varchar(32) ]
+ 	$conn = new mysqli("localhost","mysqlUserName","yourPassword",$dbname);
 	if($conn->connect_error) {
 		die("connection error : ".$conn->connect_error);
 	}
@@ -17,7 +18,7 @@
 		
 	if($conn) {
 		function checkUser($username,$authkey) {
-			$s = "select authkey from users where username='ahmad'";
+			$s = "select authkey from $usrtable where username='ahmad'";
 			$sts = $conn->query($s);
 			print "OK $sts";
 			return $sts;
@@ -28,7 +29,19 @@
 			
 			return ($result.$conn->error);
 		}
-		function getAllContacts() {}
+		function getAllContacts($conn) {
+			if($conn->connect_errno) {
+				return null;
+			}
+			$contacts = array();
+			if($conn->num_rows >0) {
+				while($tuple = $conn->fetch_assoc()) {
+					$contacts[] = array($tuple['name'],$tuple['phone'],$tuple['email']);
+				}
+			}
+			return $contacts;
+			
+		}
 		function addContact($name, $phone) {
 			$s = "insert into $contable values($name,$phone,'');";
 			$result = $conn->query($s);
